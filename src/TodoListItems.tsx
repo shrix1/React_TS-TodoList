12e726282fdd7928828c5e0f472342d09f9c1d8b
Shriprasanna B <shriprasanna32@gmail.com>
2022-09-29 00:47:36 +0530

    edit feature has bug

--- src/TodoListItems.tsx
+++ src/TodoListItems.tsx
@@ -1,53 +1,72 @@
-import React, { FC } from "react";
+import React, { FC, useState, FormEvent } from "react";
 import { Todos } from "./App"; //interface of todos (State)
 
 interface Props {
+  items: Todos;
   todo: Todos[];
   setTodo: React.Dispatch<React.SetStateAction<Todos[]>>;
 }
 
-const TodoListItems: FC<Props> = ({ todo, setTodo }) => {
+const TodoListItems: FC<Props> = ({ items, todo, setTodo }) => {
+  // const [edit, setEdit] = useState<boolean>(false);
+  // const [editCheck, setEditCheck] = useState<string>(items.todoItems);
+  // console.log(editCheck);
+
   const deleteItems = (ids: number) => {
-    setTodo(todo.filter((i: Todos) => i.id !== ids));
+    setTodo(todo.filter((i) => i.id !== ids));
   };
 
   const finishedItems = (ids: number) => {
     setTodo(todo.map((i) => (i.id === ids ? { ...i, isDone: !i.isDone } : i)));
   };
 
-  const editTodos = (ids: number) => {
-    setTodo(todo.map((i) => i));
-  };
+  // const editFunctionality = (e: FormEvent<HTMLFormElement>, ids: number) => {
+  //   e.preventDefault();
+  //   setTodo(
+  //     todo.map((i) => (i.id === ids ? { ...i, todoItems: editCheck } : i))
+  //   );
+  //   setEdit(false);
+  // };
 
   return (
-    <div className="h-[50vh] mt-[20px] text-white p-2">
-      {todo.map((items, key) => {
-        return (
-          <section key={key}>
-            <form
-              onSubmit={(e) => e.preventDefault()}
-              className="w-[400px] border-2 flex justify-between 
+    <div className="text-white p-2">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        // onSubmit={(e) => editFunctionality(e, items.id)}
+        className="w-[400px] border-2 flex justify-between 
             p-4 rounded-md mb-2 bg-slate-700"
-              style={{
-                borderColor: items.isDone ? "green" : "white",
-                opacity: items.isDone ? 0.5 : 1,
-              }}
-            >
-              {false ? (
-                <input type="text" value={items.todoItems} />
-              ) : (
-                <h1>{items.todoItems}</h1>
-              )}
+        style={{
+          borderColor: items.isDone ? "green" : "white",
+          opacity: items.isDone ? 0.5 : 1,
+        }}
+      >
+        {/* {edit ? (
+          <input
+            className="text-black"
+            value={editCheck}
+            onChange={(e) => setEditCheck(e.target.value)}
+          />
+        ) : (
+          <h1>{items.todoItems}</h1>
+        )} */}
 
-              <div className="flex gap-2">
-                <button onClick={() => editTodos(items.id)}>edit</button>
-                <button onClick={() => finishedItems(items.id)}>done</button>
-                <button onClick={() => deleteItems(items.id)}>del</button>
-              </div>
-            </form>
-          </section>
-        );
-      })}
+        <h1>{items.todoItems}</h1>
+        <h1>{items.id}</h1>
+
+        <div className="flex gap-2">
+          {/* <button
+            onClick={() => {
+              if (!edit && !items.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          >
+            edit
+          </button> */}
+          <button onClick={() => finishedItems(items.id)}>done</button>
+          <button onClick={() => deleteItems(items.id)}>del</button>
+        </div>
+      </form>
     </div>
   );
 };
